dcb4424cea8f6402be23bda28385ff39d8e62311
Dolan Miu <dolan_miu@hotmail.com>
2017-04-23 20:51:29 +0100

    updated interface

--- my-typings/alexa-voice-service/index.d.ts
+++ my-typings/alexa-voice-service/index.d.ts
@@ -1,3 +1,13 @@
-declare interface AVS {
+declare module "alexa-voice-service" {
+    interface AVSParams {
+        debug: boolean,
+        clientId: string,
+        clientSecret: string,
+        deviceId: string,
+        refreshToken: string,
+    }
 
+    class AVS {
+        constructor(params: AVSParams);
+    }
 }

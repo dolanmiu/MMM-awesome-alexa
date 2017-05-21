f3ba41a9eca42684e97b59d65fb8918113cdf882
Dolan <dolan_miu@hotmail.com>
2017-05-21 23:21:52 +0100

    Cleaned up Renderer

--- src/renderer/alexa-mirror.ts
+++ src/renderer/alexa-mirror.ts
@@ -1,36 +1,19 @@
-import { AVSWrapper } from "./avs-wrapper";
-import { AlexaStateMachine } from "./states/alexa-state-machine";
-import { VADWrapper } from "./vad-wrapper";
 // import { RainbowVisualizer } from "./visualizer/rainbow-visualizer";
-import { Visualizer } from "./visualizer/visualizer";
+// import { Visualizer } from "./visualizer/visualizer";
 
 export class AlexaMirror {
-    private avsWrapper: AVSWrapper;
-    private vadWrapper: VADWrapper;
-    private visualizer: Visualizer;
-    private alexaStateMachine: AlexaStateMachine;
+    // private visualizer: Visualizer;
 
     constructor(private mainDiv: HTMLElement, canvas: HTMLCanvasElement, config: Config) {
-        this.avsWrapper = new AVSWrapper(config);
-        this.vadWrapper = new VADWrapper();
         // this.visualizer = new RainbowVisualizer(canvas, this.avsWrapper.AudioContext);
-
-        this.alexaStateMachine = new AlexaStateMachine({
-            avs: this.avsWrapper,
-            vad: this.vadWrapper,
-            visualizer: this.visualizer,
-            div: mainDiv,
-        });
     }
 
     public start(): void {
-        this.avsWrapper.init();
-        this.vadWrapper.init();
         // this.visualizer.init();
     }
 
     public receivedNotification<T>(type: NotificationType, payload: T): void {
-        this.alexaStateMachine.broadcast(type, payload);
+        // TODO
     }
 
     public listening(): void {
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,6 +1,4 @@
 import { AlexaMirror } from "./alexa-mirror";
-import { AVSWrapper } from "./avs-wrapper";
-import { VADWrapper } from "./vad-wrapper";
 import { RainbowVisualizer } from "./visualizer/rainbow-visualizer";
 
-export { AlexaMirror, AVSWrapper, VADWrapper, RainbowVisualizer };
+export { AlexaMirror, RainbowVisualizer };

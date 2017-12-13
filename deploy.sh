6f1c59aae0168a67fb03d15bf7279d52aeee7bda
Dolan <dolan_miu@hotmail.com>
2017-12-13 02:11:09 +0000

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git add dist
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+git push

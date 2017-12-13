f896300f3a5788e0a9a23677488b5c70f69506ed
Dolan <dolan_miu@hotmail.com>
2017-12-13 20:01:24 +0000

    Improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+echo "Deploying"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add dist
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-git push
+git remote add origin https://${GH_TOKEN}@github.com/dolanmiu/MMM-awesome-alexa.git > /dev/null 2>&1
+echo "Pushing to master"
+git push -u origin master

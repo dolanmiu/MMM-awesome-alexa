d2cd91b38c7d71917427781b969759377d49cad7
Dolan <dolan_miu@hotmail.com>
2017-12-14 22:15:03 +0000

    More logging and using my own email

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 echo "Deploying"
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+echo "Setting credentials"
+git config --global user.email "dolan_miu@hotmail.com"
+git config --global user.name "Dolan Miu"
+echo "Adding dist folder"
 git add dist
+echo "Commiting"
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+echo "Add origin"
 git remote add origin https://${GH_TOKEN}@github.com/dolanmiu/MMM-awesome-alexa.git > /dev/null 2>&1
 echo "Pushing to master"
 git push -u origin master

1475e0f5d4d9d2f274f3ee6bf29cead7e20ce7cd
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-12-25 00:19:27 +0200

    Remove unused property

--- config/webpack.main.js
+++ config/webpack.main.js
@@ -8,7 +8,6 @@ module.exports = {
         path: path.resolve(__dirname, "../dist"),
         filename: "bundle.js",
         publicPath: "./",
-        chunkFilename: "[id].chunk.js",
         libraryTarget: "var",
         library: "AlexaVoiceService",
     },

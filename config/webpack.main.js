374c89420e853ba6b809472b62efe61e30364f1c
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-12-25 00:30:59 +0200

    Remove libraryTarget because it is using default value

--- config/webpack.main.js
+++ config/webpack.main.js
@@ -8,7 +8,6 @@ module.exports = {
         path: path.resolve(__dirname, "../dist"),
         filename: "bundle.js",
         publicPath: "./",
-        libraryTarget: "var",
         library: "AlexaVoiceService",
     },
 

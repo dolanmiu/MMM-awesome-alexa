fb29c1bd98dd1cb8be3c1bf59ccfbef96c1fff09
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-12-27 18:26:06 +0200

    Generate one front end bundle with webpack without js files

---
+++ src/MMM-awesome-alexa.ts
@@ -0,0 +1,96 @@
+declare const Module: {
+  register(moduleName: string, moduleProperties: object): void,
+};
+
+declare const Log: {
+  log(text: string): void,
+};
+
+enum AlexaNotification {
+  Idle = "idle",
+  Listening = "listening",
+  Busy = "busy",
+  Speaking = "speak",
+}
+
+import {AlexaMirror} from "./renderer";
+
+let alexaMirror: AlexaMirror;
+const texts: Array<string> = [];
+
+Module.register("MMM-awesome-alexa", {
+
+    // Default module config.
+    defaults: {
+        clientId: "amzn1.application-oa2-client.81574bebfb25437595d7376f44b54f8e",
+        clientSecret: "87d49f998b3a6507b8e6a08760cda274e1d44a22a2bebade9433b1e7445d66a5",
+        deviceId: "magic_mirror_alexa",
+        lite: false,
+    },
+
+    start: function () {
+        if (this.config.refreshToken === undefined) {
+            texts.push("Refresh token must be set in the config before using awesome-alexa!");
+        }
+        this.sendSocketNotification("CONFIG", this.config);
+    },
+
+    getDom: function () {
+        const alexaWrapper = document.createElement("div");
+        alexaWrapper.setAttribute("id", "wrapper");
+        alexaWrapper.classList.add("wrapper");
+        const spinner = this.createLoadingSpinner();
+        const alexaCircle = document.createElement("div");
+        alexaCircle.classList.add("alexa-circle");
+        alexaWrapper.appendChild(spinner);
+        alexaWrapper.appendChild(alexaCircle);
+
+        if (texts.length > 0) {
+            alexaWrapper.classList.add("wrapper-error");
+
+            for (const text of texts) {
+                alexaWrapper.appendChild(document.createTextNode(text));
+            }
+        }
+
+        alexaMirror = new AlexaMirror(alexaWrapper, undefined, this.config, (event, payload) => {
+            this.sendSocketNotification(event, payload);
+        }, alexaCircle);
+
+        alexaMirror.start();
+        return alexaWrapper;
+    },
+
+    getScripts: function () {
+        return [
+            this.file("dist/bundle.js"),
+        ];
+    },
+
+    getStyles: function () {
+        return [
+            this.file("styles/global.css"),
+        ];
+    },
+
+    socketNotificationReceived: function<T>(notification: AlexaNotification, payload: T): void {
+        Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+        alexaMirror.receivedNotification(notification, payload);
+    },
+
+    createLoadingSpinner: function () {
+        var img = document.createElement("img");
+        img.setAttribute('src', 'modules/MMM-awesome-alexa/styles/loading.gif');
+        img.setAttribute('id', 'loading-spinner');
+        img.classList.add('loading-spinner');
+        img.classList.add('hidden');
+        return img;
+    },
+
+    createCanvas: function () {
+        const canvas = document.createElement("canvas");
+        canvas.width = 400;
+        canvas.height = 300;
+        return canvas;
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+
+module.exports = {
+    entry: "./src/MMM-awesome-alexa.ts",
+    output: {
+        path: path.resolve(__dirname, "."),
+        filename: "MMM-awesome-alexa.js",
+    },
+
+    resolve: {
+        extensions: [".ts"],
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ["awesome-typescript-loader"],
+            }
+        ],
+    },
+};

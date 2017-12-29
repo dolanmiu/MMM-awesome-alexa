313350cb969053ac8db4cb074e4a7ef9a19dcb5e
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-12-29 23:56:20 +0200

    Try tabWidth 4

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,90 +9,92 @@ import { RendererCommunicator } from "./renderer-communicator";
 import { AlexaStateMachine } from "./states/alexa-state-machine";
 
 const checkConfig = (uncheckedConfig: UncheckedConfig): Config => {
-  if (!uncheckedConfig.clientId) {
-    throw new Error("clientId must be defined");
-  }
+    if (!uncheckedConfig.clientId) {
+        throw new Error("clientId must be defined");
+    }
 
-  if (!uncheckedConfig.clientSecret) {
-    throw new Error("clientSecret must be defined");
-  }
+    if (!uncheckedConfig.clientSecret) {
+        throw new Error("clientSecret must be defined");
+    }
 
-  if (!uncheckedConfig.deviceId) {
-    throw new Error("deviceId must be defined");
-  }
+    if (!uncheckedConfig.deviceId) {
+        throw new Error("deviceId must be defined");
+    }
 
-  if (!uncheckedConfig.refreshToken) {
-    throw new Error("refreshToken must be defined");
-  }
+    if (!uncheckedConfig.refreshToken) {
+        throw new Error("refreshToken must be defined");
+    }
 
-  if (!uncheckedConfig.wakeWord) {
-    throw new Error("wakeWord must be defined");
-  }
+    if (!uncheckedConfig.wakeWord) {
+        throw new Error("wakeWord must be defined");
+    }
 
-  return {
-    wakeWord: uncheckedConfig.wakeWord,
-    clientId: uncheckedConfig.clientId,
-    clientSecret: uncheckedConfig.clientSecret,
-    deviceId: uncheckedConfig.deviceId,
-    refreshToken: uncheckedConfig.refreshToken,
-    lite: uncheckedConfig.lite || false,
-  };
+    return {
+        wakeWord: uncheckedConfig.wakeWord,
+        clientId: uncheckedConfig.clientId,
+        clientSecret: uncheckedConfig.clientSecret,
+        deviceId: uncheckedConfig.deviceId,
+        refreshToken: uncheckedConfig.refreshToken,
+        lite: uncheckedConfig.lite || false,
+    };
 };
 
 declare const NodeHelper: {
-  create(config: {
-    start(): void;
-    socketNotificationReceived<T>(notification: NotificationType, payload: T): void;
-    [key: string]: {};
-  }): void;
+    create(config: {
+        start(): void;
+        socketNotificationReceived<T>(notification: NotificationType, payload: T): void;
+        [key: string]: {};
+    }): void;
 };
 
 module.exports = NodeHelper.create({
-  start(): void {
-    this.expressApp.get("/output.mpeg", (req: Request, res: Response): void => {
-      res.setHeader("Expires", new Date().toUTCString());
-      const outputPath = path.resolve(__dirname, "temp/output.mpeg");
+    start(): void {
+        this.expressApp.get("/output.mpeg", (req: Request, res: Response): void => {
+            res.setHeader("Expires", new Date().toUTCString());
+            const outputPath = path.resolve(__dirname, "temp/output.mpeg");
 
-      if (!fs.existsSync(outputPath)) {
-        fs
-          .createReadStream(path.resolve(__dirname, "resources/alexa/sorry-im-not-sure.mpeg"))
-          .pipe(res);
-        return;
-      }
+            if (!fs.existsSync(outputPath)) {
+                fs
+                    .createReadStream(
+                        path.resolve(__dirname, "resources/alexa/sorry-im-not-sure.mpeg"),
+                    )
+                    .pipe(res);
+                return;
+            }
 
-      fs.createReadStream(outputPath).pipe(res);
-    });
-  },
+            fs.createReadStream(outputPath).pipe(res);
+        });
+    },
 
-  socketNotificationReceived<T>(notification: NotificationType, payload: T): void {
-    if (notification === "CONFIG") {
-      const config = checkConfig(payload);
-      const configService = new ConfigService(config);
-      this.rendererCommunicator = new RendererCommunicator();
-      this.alexaStateMachine = new AlexaStateMachine({
-        audioService: new AudioService(),
-        configService: configService,
-        rendererSend: (event: NotificationType, callbackPayload: object) => {
-          this.sendSocketNotification(event, callbackPayload);
-        },
-        rendererCommunicator: this.rendererCommunicator,
-        models: new AlexaModels(config.wakeWord),
-      });
+    socketNotificationReceived<T>(notification: NotificationType, payload: T): void {
+        if (notification === "CONFIG") {
+            const config = checkConfig(payload);
+            const configService = new ConfigService(config);
+            this.rendererCommunicator = new RendererCommunicator();
+            this.alexaStateMachine = new AlexaStateMachine({
+                audioService: new AudioService(),
+                configService: configService,
+                rendererSend: (event: NotificationType, callbackPayload: object) => {
+                    this.sendSocketNotification(event, callbackPayload);
+                },
+                rendererCommunicator: this.rendererCommunicator,
+                models: new AlexaModels(config.wakeWord),
+            });
 
-      const tokenService = new TokenService({
-        refreshToken: config.refreshToken,
-        clientId: config.clientId,
-        clientSecret: config.clientSecret,
-        deviceId: config.deviceId,
-        redirectUrl: "",
-      });
+            const tokenService = new TokenService({
+                refreshToken: config.refreshToken,
+                clientId: config.clientId,
+                clientSecret: config.clientSecret,
+                deviceId: config.deviceId,
+                redirectUrl: "",
+            });
 
-      tokenService.Observable.subscribe(token => {
-        configService.Config.accessToken = token.access_token;
-      });
-      return;
-    }
+            tokenService.Observable.subscribe(token => {
+                configService.Config.accessToken = token.access_token;
+            });
+            return;
+        }
 
-    this.rendererCommunicator.sendNotification(notification);
-  },
+        this.rendererCommunicator.sendNotification(notification);
+    },
 });
--- src/main/states/idle.state.ts
+++ src/main/states/idle.state.ts
@@ -6,37 +6,37 @@ import { IStateMachineComponents } from "./alexa-state-machine";
 import { State } from "./base.state";
 
 export class IdleState extends State {
-  private detectorSubscription: Subscription;
+    private detectorSubscription: Subscription;
 
-  constructor(components: IStateMachineComponents) {
-    super(components, "idle");
-  }
+    constructor(components: IStateMachineComponents) {
+        super(components, "idle");
+    }
 
-  public onEnter(): void {
-    this.components.rendererSend("idle", {});
-    this.components.detector = new HotwordDetector(this.components.models);
-    this.components.mic = this.createMic();
-    // tslint:disable-next-line:no-any
-    this.components.mic.pipe(this.components.detector as any);
-    this.detectorSubscription = this.components.detector.Observable.subscribe(value => {
-      switch (value) {
-        case DETECTOR.Hotword:
-          this.transition(this.allowedStateTransitions.get("listening"));
-          break;
-      }
-    });
-  }
+    public onEnter(): void {
+        this.components.rendererSend("idle", {});
+        this.components.detector = new HotwordDetector(this.components.models);
+        this.components.mic = this.createMic();
+        // tslint:disable-next-line:no-any
+        this.components.mic.pipe(this.components.detector as any);
+        this.detectorSubscription = this.components.detector.Observable.subscribe(value => {
+            switch (value) {
+                case DETECTOR.Hotword:
+                    this.transition(this.allowedStateTransitions.get("listening"));
+                    break;
+            }
+        });
+    }
 
-  public onExit(): void {
-    this.detectorSubscription.unsubscribe();
-  }
+    public onExit(): void {
+        this.detectorSubscription.unsubscribe();
+    }
 
-  private createMic(): record.Mic {
-    const mic = record.start({
-      threshold: 0,
-      verbose: false,
-    });
+    private createMic(): record.Mic {
+        const mic = record.start({
+            threshold: 0,
+            verbose: false,
+        });
 
-    return mic;
-  }
+        return mic;
+    }
 }

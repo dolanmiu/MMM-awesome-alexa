951fdc0a81ab4d5807143e95009c67935138ce69
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-12-29 11:54:56 +0200

    Remove unused import

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,54 @@
 const nodeExternals = require("webpack-node-externals");
-const webpack = require("webpack");
 const path = require("path");
 
 const clientConfig = {
-    entry: "./src/renderer/MMM-awesome-alexa.ts",
-    output: {
-        path: __dirname,
-        filename: "MMM-awesome-alexa.js",
-    },
+  entry: "./src/renderer/MMM-awesome-alexa.ts",
+  output: {
+    path: __dirname,
+    filename: "MMM-awesome-alexa.js",
+  },
 
-    resolve: {
-        extensions: [".ts"],
-    },
+  resolve: {
+    extensions: [".ts"],
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.ts$/,
-                include: [
-                    path.resolve(__dirname, "src/renderer"),
-                ],
-                loaders: ["awesome-typescript-loader"],
-            }
-        ],
-    },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, "src/renderer")],
+        loaders: ["awesome-typescript-loader"],
+      },
+    ],
+  },
 };
 const serverConfig = {
-    target: "node",
-    externals: [nodeExternals()],
-    entry: "./src/main/index.ts",
-    output: {
-        path: __dirname,
-        filename: "node_helper.js",
-        libraryTarget: "commonjs2",
-        library: "yourLibName",
-    },
-    node: {
-        __dirname: false
-    },
+  target: "node",
+  externals: [nodeExternals()],
+  entry: "./src/main/index.ts",
+  output: {
+    path: __dirname,
+    filename: "node_helper.js",
+    libraryTarget: "commonjs2",
+    library: "yourLibName",
+  },
+  node: {
+    __dirname: false,
+  },
 
-    resolve: {
-        extensions: [".ts"],
-    },
+  resolve: {
+    extensions: [".ts"],
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.ts$/,
-                include: [
-                    path.resolve(__dirname, "src/main"),
-                ],
-                loaders: ["awesome-typescript-loader"],
-            }
-        ],
-    },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, "src/main")],
+        loaders: ["awesome-typescript-loader"],
+      },
+    ],
+  },
 };
 
 module.exports = [serverConfig, clientConfig];

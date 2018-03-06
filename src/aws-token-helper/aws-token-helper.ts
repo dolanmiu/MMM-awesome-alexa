da468b7e8b14a2773a4a6a0740e51270e8bf9387
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-03-06 18:41:20 +0200

    Ask user for clientId

--- src/aws-token-helper/aws-token-helper.ts
+++ src/aws-token-helper/aws-token-helper.ts
@@ -3,17 +3,42 @@ import open = require("open");
 
 const generateQuery = (params: { [key: string]: string }) =>
     Object.keys(params)
-        .map((key: string) => key + "=" + params[key])
+        .map((key: string) => key + "=" + encodeURIComponent(params[key]))
         .join("&");
 
-const getParams = generateQuery({
-    client_id: "lol",
-    scope: "lol",
-    scope_data: "lol",
-    response_type: "lol",
-    redirect_uri: "lol",
+function prompt(question: string, callback: (input: string) => void): void {
+    const stdin = process.stdin;
+    const stdout = process.stdout;
+
+    stdin.resume();
+    stdout.write(question);
+
+    stdin.once("data", data => {
+        callback(data.toString().trim());
+    });
+}
+
+prompt("Client ID? ", (clientId: string) => {
+    const productId = "MagicMirror";
+    const deviceSerialNumber = 123; // can be anything
+    const scopeData = {
+        "alexa:all": {
+            productID: productId,
+            productInstanceAttributes: {
+                deviceSerialNumber: deviceSerialNumber,
+            },
+        },
+    };
+    const getParams = generateQuery({
+        client_id: clientId,
+        scope: "alexa:all",
+        scope_data: JSON.stringify(scopeData),
+        response_type: "code",
+        redirect_uri: "https://localhost:9745/authresponse",
+    });
+
+    const authUrl = `https://www.amazon.com/ap/oa?${getParams}`;
+
+    open(authUrl);
+    process.exit();
 });
-
-const authUrl = `https://www.amazon.com/ap/oa?${getParams}`;
-
-open(authUrl);

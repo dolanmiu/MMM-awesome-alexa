b6709a2153d0be4410963917392ad6ab914f2179
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-03-06 19:06:38 +0200

    Make script easier to understand

--- src/aws-token-helper/aws-token-helper.ts
+++ src/aws-token-helper/aws-token-helper.ts
@@ -20,29 +20,41 @@ function prompt(question: string): Promise<string> {
     });
 }
 
-prompt("Client ID?").then(clientId => {
-    prompt("Product Id?").then(productId => {
-        prompt("Redirect URI (allowed return URL)?").then(redirectURI => {
-            const scopeData = {
-                "alexa:all": {
-                    productID: productId,
-                    productInstanceAttributes: {
-                        deviceSerialNumber: 123, // can be anything
-                    },
-                },
-            };
-            const getParams = generateQuery({
-                client_id: clientId,
-                scope: "alexa:all",
-                scope_data: JSON.stringify(scopeData),
-                response_type: "code",
-                redirect_uri: redirectURI,
+console.log("Welcome to the Amazon auth helper!");
+console.log("I will need three things from you:");
+console.log("- Client ID");
+console.log("- Product ID");
+console.log("- Allowed return URL");
+
+prompt("[Press any key to continue]").then(() => {
+    prompt("Client ID?").then(clientId => {
+        prompt("Product Id?").then(productId => {
+            prompt("Redirect URI (allowed return URL)?").then(redirectURI => {
+                prompt(
+                    "Great! Next I will now open a browser where you can authorize your Alexa product\n[Press any key to continue]",
+                ).then(() => {
+                    const scopeData = {
+                        "alexa:all": {
+                            productID: productId,
+                            productInstanceAttributes: {
+                                deviceSerialNumber: 123, // can be anything
+                            },
+                        },
+                    };
+                    const getParams = generateQuery({
+                        client_id: clientId,
+                        scope: "alexa:all",
+                        scope_data: JSON.stringify(scopeData),
+                        response_type: "code",
+                        redirect_uri: redirectURI,
+                    });
+
+                    const authUrl = `https://www.amazon.com/ap/oa?${getParams}`;
+
+                    open(authUrl);
+                    process.exit();
+                });
             });
-
-            const authUrl = `https://www.amazon.com/ap/oa?${getParams}`;
-
-            open(authUrl);
-            process.exit();
         });
     });
 });
